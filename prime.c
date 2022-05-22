79cb1d50fa82b79133c863470f9b615c0ae79e6a
Sansyo-san <yuta0616a@gmail.com>
2022-05-22 22:03:13 +0900

    二回目のコミット

--- prime.c
+++ prime.c
@@ -3,46 +3,46 @@
 
 int main(void)
 {
-    int i, j, k, c;
-    static int a[N];
+	int i, j, k, c;
+	static int a[N];
 
-    for (i = 2; i < N; i++)
-    {
-        a[i] = 0; // â¿(a)‚Ì‰Šú‰»,‰ü—Ç‚Q
-    }
+	for (i = 2; i < N; i++)
+	{
+		a[i] = 0; // â¿(a)‚Ì‰Šú‰»,‰ü—Ç‚Q
+	}
 
-    for (i = 2; i < N / 2; i++)
-        if (!a[i])
-        {
-            j = 2 * i;
-            while (j < N) 
-            { 
-                a[j] = 1;
-                j += i; 
-            } // i‚Ì”{”‚ğœ‚­
-        }
+	for (i = 2; i < N / 2; i++)
+		if (!a[i])
+		{
+			j = 2 * i;
+			while (j < N)
+			{
+				a[j] = 1;
+				j += i;
+			} // i‚Ì”{”‚ğœ‚­
+		}
 
-    k = 0;
-    c = 0;
+	k = 0;
+	c = 0;
 
-    for (i = 2; i < N; i++)
-        if (!a[i]) 
-        {
-            c++;//‰ü—Ç‚T
-            if (i >= N - 1001)//‰ü—Ç‚R
-            {
-                 printf("%7d,", i);
-                 if (++k % 5 == 0) 
-                 printf("\n"); // ‚TŒÂ‚Å‰üsA‰ü—Ç‚S
-            }
-           
-        }
-    if (k % 5 != 0)
-    {
-        printf("\n");
-    }
+	for (i = 2; i < N; i++)
+		if (!a[i])
+		{
+			c++;//‰ü—Ç‚T
+			if (i >= N - 1001)//‰ü—Ç‚R
+			{
+				printf("%7d,", i);
+				if (++k % 5 == 0)
+					printf("\n"); // ‚TŒÂ‚Å‰üsA‰ü—Ç‚S
+			}
 
-    printf("2‚©‚ç100–œ1‚Ü‚Å‚Ì‘f”‚Ì”‚Í%d", c);
+		}
+	if (k % 5 != 0)
+	{
+		printf("\n");
+	}
 
-    return 0;
+	printf("2‚©‚ç100–œ1‚Ü‚Å‚Ì‘f”‚Ì”‚Í%d", c);
+
+	return 0;
 }
